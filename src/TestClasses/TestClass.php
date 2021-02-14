568a2e6408ef0f077984386f2b537f18b3a1fa97
Anpanator <nickschradick@googlemail.com>
2021-02-14 20:31:13 +0100

    Initial commit

---
+++ src/TestClasses/TestClass.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmolCms\TestClasses;
+
+
+class TestClass
+{
+    public function naice(): string
+    {
+        return "Hello World";
+    }
+}
---
+++ src/index.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+use SmolCms\TestClasses\TestClass;
+
+require_once '../vendor/autoload.php';
+
+$testClass = new TestClass();
+
+echo $testClass->naice();
