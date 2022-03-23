5e59de686669de19b787e7ceab1295e05381ee6d
Anpanator <nickschradick@googlemail.com>
2022-03-23 22:00:10 +0100

    Utilize readonly modifier in Request and Url classes

--- src/SmolCms/Data/Business/Url.php
+++ src/SmolCms/Data/Business/Url.php
@@ -11,25 +11,17 @@ use SmolCms\Service\Validation\Attribute\ValidateRange;
 
 class Url
 {
-
-    /**
-     * Url constructor.
-     * @param string $protocol
-     * @param string $host
-     * @param int|null $port
-     * @param string|null $path
-     * @param string|null $query
-     */
     public function __construct(
         #[ValidateAllowList(['http', 'https'])]
         public readonly string $protocol,
         #[ValidateHost(ValidateHost::IPV4 | ValidateHost::IPV6 | ValidateHost::DOMAIN)]
-        public readonly  string $host,
+        public readonly string $host,
         #[ValidateRange(min: 1, max: 65535)]
         public readonly ?int $port = null,
         public readonly ?string $path = null,
         public readonly ?string $query = null,
-    ) {
+    )
+    {
     }
 
     public function __toString(): string
